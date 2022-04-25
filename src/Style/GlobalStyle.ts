abfabd0e90737c1e9f9f21f47d9d5a71a7b13c59
PHnrq <paulofigueredo7@gmail.com>
2022-04-25 16:38:10 -0300

    clean file

---
+++ src/Style/GlobalStyle.ts
@@ -0,0 +1,23 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        //Reset
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        border: none;
+        //Colors
+        --strong-cyan: hsl(172, 67%, 45%);
+        --very-dark-cyan: hsl(183, 100%, 15%);
+        --dark-grayish-cyan: hsl(186, 14%, 43%);
+        --grayish-cyan: hsl(184, 14%, 56%);
+        --light-grayish-cyan: hsl(185, 41%, 84%);
+        --very-light-grayish-cyan: hsl(189, 41%, 97%);
+        --white: hsl(0, 0%, 100%);
+        //Fonts
+        font-family: 'Space Mono', monospace;
+        font-size: 62.5%;
+    }
+`
