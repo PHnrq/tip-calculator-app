15c77c226fd592bc8996fe58949e64f70414d1cd
PHnrq <paulofigueredo7@gmail.com>
2022-05-12 08:21:17 -0300

    Remove o globalStyle pra teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Main } from "./components/Main";
-import  GlobalStyle  from "./style/GlobalStyle";
+//import  GlobalStyle  from "./style/GlobalStyle";
 
 export function App() {
   return (
@@ -9,7 +9,7 @@ export function App() {
         <br />
         tter
       </h1>
-      <GlobalStyle />
+      {/* <GlobalStyle /> */}
       <Main />
     </div>
   );
