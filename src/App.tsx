aa7a3a940359996481f50fbc8672613b17751612
PHnrq <paulofigueredo7@gmail.com>
2022-05-12 08:25:28 -0300

    adiciona o globalStyle pra testar em produção

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Main } from "./components/Main";
-//import  GlobalStyle  from "./style/GlobalStyle";
+import GlobalStyle from "./style/GlobalStyle";
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
      <h1>
         Spli
         <br />
         tter
       </h1>
-      {/* <GlobalStyle /> */}
       <Main />
     </div>
   );
